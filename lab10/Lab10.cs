f6b1b7873b22599af5ba1fd738c6798765ca66ee
Aleksei <fx5600@yandex.ru>
2023-12-10 17:26:01 +0500

    upd lab 10

---
+++ lab10/Lab10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Lab10 {
+    public class Lab10 {
+        static void Main() {
+            const int variantNumber = 549 % 16 - 1; //номер варианта 8
+            Console.WriteLine($"Номер варианта = {variantNumber}\n");
+
+            //var org = new Organisation();
+            //var ins = new InsuranceCompany();
+            //ins.Show();
+            //var fact = new Factory();
+            //fact.Show();
+
+            var sh = new Shipyard();
+            sh.RandomInit();
+            sh.Show();
+        }
+    }
+}
