22943326944a97065080f2eb8e7e5592ad9243e4
Aleksei <fx5600@yandex.ru>
2023-12-24 00:03:29 +0500

    upd lab10

---
+++ lab10/TestLab10/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab10;
+
+namespace TestLab10 {
+    [TestClass]
+    public class TestOrganization {
+        [TestMethod]
+        public void TestZeroOrganisation() {
+            var org = new Organisation(null, null, 0);
+            var testOrg = new Organisation();
+            Assert.AreEqual(org, testOrg);
+        }
+    }
+}
