95411d79136d9af625e20cdcb38ec36d9ba59aed
Aleksei <fx5600@yandex.ru>
2024-03-20 14:38:06 +0500

    add lab12

---
+++ Lab12/Lab12_1/MyList.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab12_1;
+
+public class MyList<T>
+{
+    private Point<T> _begin = null;
+
+    public int Length
+    {
+        get
+        {
+            Point<T> p = _begin;
+            int len = 0;
+            while (p != null)
+            {
+                p = p._next;
+                len++;
+            }
+
+            return len;
+        }
+    }
+
+    public MyList(uint size)
+    {
+        _begin = new Point<T>();
+        Point<T> p = _begin;
+        for (int i = 1; i < size; i++)
+        {
+            var temp = new Point<T>();
+            p._next = temp;
+            p = temp;
+        }
+    }
+
+    public MyList(params T[] arr)
+    {
+        _begin = new Point<T>();
+        Point<T> p = _begin;
+        _begin._data = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            var temp = new Point<T>(arr[i]);
+            p._next = temp;
+            p = temp;
+        }
+    }
+}
---
+++ Lab12/Lab12_1/Point.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace Lab12_1;
+
+public class Point<T>
+{
+    public T _data;
+    public Point<T> _next;
+    public Point<T> _prev;
+
+    public Point()
+    {
+        _data = default;
+        _next = null;
+        _prev = null;
+    }
+
+    public Point(T data)
+    {
+        _data = data;
+        _next = null;
+        _prev = null;
+    }
+
+    public override string ToString()
+    {
+        return _data + " ";
+    }
+}
