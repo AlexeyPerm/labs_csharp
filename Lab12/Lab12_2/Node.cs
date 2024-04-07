de61402ff9f7d9159447f5f98ebe8b80fa804d7b
Aleksei <fx5600@yandex.ru>
2024-04-07 23:39:59 +0500

    add lab12_2

---
+++ Lab12/Lab12_2/Node.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibraryLab10;
+
+namespace Lab12_2;
+
+public class Node
+{
+    private Organisation _data;
+    private Organisation _left;
+    private Organisation _right;
+
+    public Node()
+    {
+        _data = new Organisation();
+        _left = null;
+        _right = null;
+    }
+
+    /// <summary>
+    /// Рандомное заполнение элемента
+    /// </summary>
+    /// <param name="random">Рандом или не рандом?</param>
+    public Node(bool random)
+    {
+        if (!random) return;
+        _data = new Organisation();
+        _data.RandomInit();
+        _left = null;
+        _right = null;
+    }
+
+    public Node(Organisation data)
+    {
+        _data = data;
+        _left = null;
+        _right = null;
+    }
+
+    public override string ToString()
+    {
+        return _data.ToString();
+    }
+}
---
+++ Lab12/Lab12_2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab12_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
