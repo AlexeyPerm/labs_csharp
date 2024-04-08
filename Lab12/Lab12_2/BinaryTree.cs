5be921d052596a76110204d79aaeb2cbfa7cccf1
Alexey <fx5600@yandex.ru>
2024-04-08 10:14:04 +0500

    upd lab12_2

--- Lab12/Lab12_2/BinaryTree.cs
+++ Lab12/Lab12_2/BinaryTree.cs
@@ -1,50 +1,6 @@
-﻿using ClassLibraryLab10;
-
-namespace Lab12_2;
+﻿namespace Lab12_2;
 
 public class BinaryTree
 {
-    #region Node
-    public class Node
-    {
-        private Organisation _data;
-        private Node _left;
-        private Node _right;
-
-        public Node()
-        {
-            _data = new Organisation();
-            _left = null;
-            _right = null;
-        }
-
-        /// <summary>
-        /// Рандомное заполнение элемента
-        /// </summary>
-        /// <param name="random">Рандом или не рандом?</param>
-        public Node(bool random)
-        {
-            if (!random) return;
-            _data = new Organisation();
-            _data.RandomInit();
-            _left = null;
-            _right = null;
-        }
-
-        public Node(Organisation data)
-        {
-            _data = data;
-            _left = null;
-            _right = null;
-        }
-
-        public override string ToString()
-        {
-            return _data.ToString();
-        }
-    }
-
-    #endregion
-
-    public Node root = null;
+    public NodeTree root = null;
 }
---
+++ Lab12/Lab12_2/NodeTree.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibraryLab10;
+
+namespace Lab12_2;
+
+public class NodeTree
+{
+    private Organisation _data;
+    private NodeTree _left;
+    private NodeTree _right;
+
+    public NodeTree()
+    {
+        _data = new Organisation();
+        _left = null;
+        _right = null;
+    }
+
+    /// <summary>
+    /// Рандомное заполнение элемента
+    /// </summary>
+    /// <param name="random">Рандом или не рандом?</param>
+    public NodeTree(bool random)
+    {
+        if (!random) return;
+        _data = new Organisation();
+        _data.RandomInit();
+        _left = null;
+        _right = null;
+    }
+
+    public NodeTree(Organisation data)
+    {
+        _data = data;
+        _left = null;
+        _right = null;
+    }
+
+    public override string ToString()
+    {
+        return _data.ToString();
+    }
+}
