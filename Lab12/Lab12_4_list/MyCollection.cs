3a6cddcfff5b92790acacd5d3086fa8a31405929
Aleksei <fx5600@yandex.ru>
2024-05-11 14:34:23 +0500

    upd lab12

--- Lab12/Lab12_4_list/MyCollection.cs
+++ Lab12/Lab12_4_list/MyCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace Lab12_4_list;
 
@@ -7,6 +8,9 @@ public class MyCollection<T> : ICollection<T>
     private Node<T> Head { get; set; }
     public int Count { get; private set; }
     public bool IsReadOnly { get; }
+
+    public bool IsEmpty => Count == 0;
+    public virtual T this[int index] => FindIndex(index);
 
     public MyCollection()
     { }
@@ -27,10 +31,29 @@ public class MyCollection<T> : ICollection<T>
         }
     }
 
-    public bool IsEmpty => Count == 0;
-    
+    /// <summary>
+    /// Поиск элемента в списке
+    /// </summary>
+    /// <param name="match">Искомый элемент</param>
+    /// <returns>Возвращает найденый элемент</returns>
+    /// <exception cref="Exception">Список пуст</exception>
+    /// <exception cref="ArgumentNullException">Искомый элемент пустой</exception>
+    public T Find(T match)
+    {
+        if (Count == 0) throw new Exception("Список пустой");
+        if (match == null) throw new ArgumentNullException(nameof(match), "Нечего искать");
+        foreach (var item in this)
+        {
+            if (item.Equals(match))
+            {
+                return item;
+            }
+        }
 
-    
+        return default;
+    }
+
+
     public void CopyTo(T[] array, int arrayIndex)
     {
         ArgumentNullException.ThrowIfNull(array);
@@ -45,7 +68,6 @@ public class MyCollection<T> : ICollection<T>
         {
             array[index++ + arrayIndex] = item;
         }
-
     }
 
     /// <summary>
@@ -71,11 +93,6 @@ public class MyCollection<T> : ICollection<T>
             current = current.Next;
         } while (current != Head);
 
-        if (Count == 1)
-        {
-            Clear();
-            return true;
-        }
 
         if (removedItem == null) return false;
 
@@ -85,8 +102,19 @@ public class MyCollection<T> : ICollection<T>
         removedItem.Previous.Next = removedItem.Next;
         removedItem.Next.Previous = removedItem.Previous;
         Count--;
-
         return true;
+    }
+
+    /// <summary>
+    /// Множественное удаление данных из списка
+    /// </summary>
+    /// <param name="arr">Передаваемый массив данных</param>
+    public void RemoveMultiple(params T[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Remove(item);
+        }
     }
 
     /// <summary>
@@ -115,6 +143,18 @@ public class MyCollection<T> : ICollection<T>
     }
 
     /// <summary>
+    /// Множественное добавление элементов в список
+    /// </summary>
+    /// <param name="arr">Передаваемый массив данных</param>
+    public void AddMultiple(params T[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Add(item);
+        }
+    }
+
+    /// <summary>
     /// Очищает список
     /// </summary>
     public void Clear()
@@ -136,22 +176,6 @@ public class MyCollection<T> : ICollection<T>
         return false;
     }
 
-
-    // IEnumerator<T> IEnumerable<T>.GetEnumerator()
-    // {
-    //     return (IEnumerator<T>)GetEnumerator();
-    // }
-    //
-    // public IEnumerator GetEnumerator()
-    // {
-    //     var current = Head;
-    //     for (var i = 0; i < Count; i++)
-    //     {
-    //         yield return current;
-    //         current = current.Next;
-    //     }
-    // }
-
     public IEnumerator<T> GetEnumerator()
     {
         var current = Head;
@@ -165,5 +189,21 @@ public class MyCollection<T> : ICollection<T>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return ((IEnumerable)this).GetEnumerator();
+    }
+
+    public object Clone() => MemberwiseClone();
+
+    private T FindIndex(int index)
+    {
+        if (Count == 0) throw new Exception("Список пустой");
+        if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException(nameof(index), "Неправильный индекс");
+
+        var current = Head;
+        for (var i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        return current.data;
     }
 }
